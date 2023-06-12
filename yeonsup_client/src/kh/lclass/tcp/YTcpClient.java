2606ab3d43ec7e74d4913c2008d5b73173de4cb7
Minji0a0 <mingmingji0a0@gmail.com>
2023-06-12 11:13:18 +0900

    실습

--- yeonsup_client/src/kh/lclass/tcp/YTcpClient.java
+++ yeonsup_client/src/kh/lclass/tcp/YTcpClient.java
@@ -1,10 +1,70 @@
 package kh.lclass.tcp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class YTcpClient {
 
-	public void testYtcpClient(String Ip, int port) {
+	public void testYtcpClient(String ip, int port) {
+	Socket socket = null;
+	InputStream in = null;
+	OutputStream out = null;
+	BufferedReader br = null;
+	PrintWriter pw = null;
 	
-
+	BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+	
+	try {
+		socket = new Socket(ip , port);
+		System.out.println("서버에 접속 성공");
 		
+		in = socket.getInputStream();
+		out = socket.getOutputStream();
+		
+		br = new BufferedReader(new InputStreamReader(in));
+		pw = new PrintWriter(new OutputStreamWriter(out));
+		
+		String sendMsg = null;
+		while(true) {
+			System.out.println("메세지 >> ");
+			sendMsg = stdIn.readLine();
+			System.out.println("####" + sendMsg);
+			
+			pw.print(sendMsg);
+			pw.write(sendMsg);
+			pw.flush();
+			
+			String receivedMsg = br.readLine();
+			System.out.println("서버로부터 받은 메시지 : " + receivedMsg);
+		}
+	}catch(UnknownHostException e) {
+		e.printStackTrace();
+	}catch(IOException e) {
+			e.printStackTrace();
+	}finally {
+		try {
+				if(stdIn != null)
+					stdIn.close();
+				if(pw != null)
+					pw.close();
+				if(br != null)
+					br.close();
+				if(out != null)
+					out.close();
+				if(in != null)
+					in.close();
+				if(socket != null)
+					socket.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
--- yeonsupserver/src/kh/lclass/tcp/YTcpServer.java
+++ yeonsupserver/src/kh/lclass/tcp/YTcpServer.java
@@ -1,9 +1,69 @@
 package kh.lclass.tcp;
 
-public class YTcpServer {
-	
-	public void testYtcpServer() {
-		
-	}
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+import javax.sound.sampled.Port;
+
+public class YTcpServer {
+
+	public void testYtcpServer(int Port) {
+		ServerSocket ss = null;
+		Socket sc = null;
+		InputStream in = null;
+		OutputStream out = null;
+		BufferedReader br = null;
+		BufferedWriter wr = null;
+		try {
+			ss = new ServerSocket(Port);
+			while (true) {
+				System.out.println("클라이언트 접속 대기 중........ ");
+				sc = ss.accept();
+				System.out.println("클라이언트 접속됨 : " + sc.getLocalPort());
+				System.out.println("클라이언트 접속됨 : " + sc.getPort());
+
+				in = sc.getInputStream();
+				out = sc.getOutputStream();
+
+				br = new BufferedReader(new InputStreamReader(in));
+				wr = new BufferedWriter(new OutputStreamWriter(out));
+
+				String receivedMsg = null;
+				while ((receivedMsg = br.readLine()) != null) {
+					System.out.println("받은메시지 : " + receivedMsg);
+					wr.write(receivedMsg + " : 메시지 잘 받았음. \n");
+					wr.flush();
+
+				}
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (wr != null)
+					wr.close();
+				if (br != null)
+					br.close();
+				if (out != null)
+					out.close();
+				if (in != null)
+					in.close();
+				if (sc != null)
+					sc.close();
+				if (ss != null)
+					ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+
+			}
+		}
+	}
 }
