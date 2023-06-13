56e47fa8121b1b83225a9d31b16fbce0b347ee8c
Minji0a0 <mingmingji0a0@gmail.com>
2023-06-13 11:50:12 +0900

    20230613 입력하는 echo 메시지

--- 20230613_udp_sender/src/kh/lclass/udp/UdpEchoSender.java
+++ 20230613_udp_sender/src/kh/lclass/udp/UdpEchoSender.java
@@ -1,11 +1,14 @@
 package kh.lclass.udp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.nio.Buffer;
 
 public class UdpEchoSender {
 	public static void main(String[] args) {
@@ -19,32 +22,48 @@ public class UdpEchoSender {
 		String destName = "localhost";
 
 		DatagramSocket dSock = null;
+		BufferedReader br = null;
+		
 		try {
 			// DatatgranSocket 객체 생성
 			dSock = new DatagramSocket(myPort);// 매개인자 없으면 자동 port 번호 할당. 지정하면 해당 포트번호로 소켓 생성
 
-			// 전달할 메시지
-			String sendMsg = "안녕";
+			// system.in 기반의 스트림
+			br = new BufferedReader(new InputStreamReader(System.in));
+			while (true) {
+				// 전달할 메시지
+				System.out.println("입력>> ");
+				String  sendMsg = br.readLine(); // consile 입력받음
+//				String sendMsg = "abc";
+				if(sendMsg.equals("exit")){
+					break;
+				}
 
-			InetAddress destIp = null;
-			try {
-				// 메시지 전달
+				InetAddress destIp = null;
+				try {
+					// 메시지 전달
 //			3. 연결할 클라이언트 IP주소를 가진 InetAddress 객체 생성
-				destIp = InetAddress.getByName(destName);
+					destIp = InetAddress.getByName(destName);
 //			4. 전송할 메시지를 byte[]로 바꿈 
-				byte[] byteMsg = sendMsg.getBytes();
+					byte[] byteMsg = sendMsg.getBytes();
 //			5. 전송할 메시지를 DatagramaPacket 객체에 담음
-				DatagramPacket sendData = new DatagramPacket(byteMsg, byteMsg.length, destIp, destPort);
+					DatagramPacket sendData = new DatagramPacket(byteMsg, byteMsg.length, destIp, destPort);
 //			6. 소켓 레퍼런스를사용하여 메시지 전송
-				dSock.send(sendData);
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
+					dSock.send(sendData);
+				} catch (UnknownHostException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-		} catch (SocketException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
+			try {
+			if(br != null) br.close();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
 			if (dSock != null)
 				dSock.close();
 		}
