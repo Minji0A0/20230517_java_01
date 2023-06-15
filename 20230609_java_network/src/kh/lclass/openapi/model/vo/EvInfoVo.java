45ba1243e40e33841ecb8d8c9992e9fb6d6e85e4
Minji0a0 <mingmingji0a0@gmail.com>
2023-06-15 16:35:00 +0900

    20230615

---
+++ 20230609_java_network/src/kh/lclass/openapi/model/vo/EvInfoVo.java
@@ -0,0 +1,21 @@
+package kh.lclass.openapi.model.vo;
+
+public class EvInfoVo {
+//	private String resultCode;
+//	private String resultMsg;	
+//	private String numOfRows;	
+//	private String pageNo;	
+	private String totalCount;	
+	//
+	private String addr;	
+	private Integer chargeTp;	
+	private String cpId;	
+	private String cpNm;	
+	private String cpStat;	
+	private String cpTp;	
+	private String csId;	
+	private String csNm;	
+	private String lat;	
+	private String longi;	
+	private String statUpdateDatetime;	
+}
